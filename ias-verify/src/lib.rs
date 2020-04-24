e19a0b098bc3d61e92a00b7b6bf256d2eb3c9418
Alain Brenzikofer <alain.brenzikofer@scs.ch>
2020-04-24 17:19:03 +0200

    disabled certificate and signature checks. just parsing now. builds

--- ias-verify/src/lib.rs
+++ ias-verify/src/lib.rs
@@ -15,22 +15,68 @@
 
 */
 
-use std::io::BufReader;
-use std::ptr;
-use std::time::SystemTime;
-use std::vec::Vec;
+#![cfg_attr(not(feature = "std"), no_std)]
 
+use sp_std::prelude::*;
+use sp_std::ptr;
+use sp_io::misc::print_utf8;
 use chrono::prelude::*;
-use itertools::Itertools;
-use log::*;
+//use itertools::Itertools;
+//use log::*;
+//use serde_json::Value;
+//use sgx_types::*;
+//use sgx_ucrypto::SgxEccHandle;
+
+//use super::{SgxReport, SgxStatus};
+use codec::{Encode, Decode};
 use serde_json::Value;
-use sgx_types::*;
-use sgx_ucrypto::SgxEccHandle;
 
-use super::{SgxReport, SgxStatus};
-use codec::Encode;
+const SGX_REPORT_DATA_SIZE: usize = 64;
+#[derive(Encode, Decode, Copy, Clone)]
+pub struct SgxReportData {
+    d: [u8; SGX_REPORT_DATA_SIZE]
+}
 
+// see Intel SGX SDK https://github.com/intel/linux-sgx/blob/master/common/inc/sgx_report.h
+const SGX_REPORT_BODY_RESERVED1_BYTES: usize = 12;
+const SGX_REPORT_BODY_RESERVED2_BYTES: usize = 32;
+const SGX_REPORT_BODY_RESERVED3_BYTES: usize = 32;
+const SGX_REPORT_BODY_RESERVED4_BYTES: usize = 42;
 
+#[derive(Encode, Decode, Copy, Clone)]
+pub struct SgxReportBody {
+    cpu_svn: [u8; 16],        /* (  0) Security Version of the CPU */
+    misc_select: [u8; 4],    /* ( 16) Which fields defined in SSA.MISC */
+    reserved1: [u8; SGX_REPORT_BODY_RESERVED1_BYTES],  /* ( 20) */
+    isv_ext_prod_id: [u8; 16],  /* ( 32) ISV assigned Extended Product ID */
+    attributes: [u8; 16],       /* ( 48) Any special Capabilities the Enclave possess */
+    mr_enclave: [u8; 32],     /* ( 64) The value of the enclave's ENCLAVE measurement */
+    reserved2: [u8; SGX_REPORT_BODY_RESERVED2_BYTES],  /* ( 96) */
+    mr_signer: [u8; 32],      /* (128) The value of the enclave's SIGNER measurement */
+    reserved3: [u8; SGX_REPORT_BODY_RESERVED3_BYTES],  /* (160) */
+    config_id: [u8; 64],      /* (192) CONFIGID */
+    isv_prod_id: u16,    /* (256) Product ID of the Enclave */
+    isv_svn: u16,        /* (258) Security Version of the Enclave */
+    config_svn: u16,     /* (260) CONFIGSVN */
+    reserved4: [u8; SGX_REPORT_BODY_RESERVED4_BYTES],  /* (262) */
+    isv_family_id: [u8; 16],  /* (304) ISV assigned Family ID */
+    report_data: SgxReportData,    /* (320) Data provided by the user */    
+}
+
+// see Intel SGX SDK https://github.com/intel/linux-sgx/blob/master/common/inc/sgx_quote.h
+#[derive(Encode, Decode, Copy, Clone)]
+pub struct SgxQuote {
+    version: u16,        /* 0   */
+    sign_type: u16,      /* 2   */
+    epid_group_id: u32,  /* 4   */
+    qe_svn: u16,         /* 8   */
+    pce_svn: u16,        /* 10  */
+    xeid: u32,           /* 12  */
+    basename: [u8; 32],   /* 16  */
+    report_body: SgxReportBody, /* 48  */
+    signature_len: u32,    /* 432 */
+    signature: [u8; 64]    /* 436 */  //must be hard-coded for SCALE codec
+}
 
 #[derive(Encode, Decode, Copy, Clone, PartialEq)]
 #[cfg_attr(feature = "std", derive(Debug))]
@@ -56,18 +102,13 @@ pub struct SgxReport {
     pub timestamp: i64,
 }
 
-
-
 type SignatureAlgorithms = &'static [&'static webpki::SignatureAlgorithm];
-
 static SUPPORTED_SIG_ALGS: SignatureAlgorithms = &[
-    &webpki::ECDSA_P256_SHA256,
-    &webpki::ECDSA_P256_SHA384,
-    &webpki::ECDSA_P384_SHA256,
-    &webpki::ECDSA_P384_SHA384,
-    &webpki::RSA_PSS_2048_8192_SHA256_LEGACY_KEY,
-    &webpki::RSA_PSS_2048_8192_SHA384_LEGACY_KEY,
-    &webpki::RSA_PSS_2048_8192_SHA512_LEGACY_KEY,
+    // TODO: we will need ECDSA!
+    // &webpki::ECDSA_P256_SHA256,
+    // &webpki::ECDSA_P256_SHA384,
+    // &webpki::ECDSA_P384_SHA256,
+    // &webpki::ECDSA_P384_SHA384,
     &webpki::RSA_PKCS1_2048_8192_SHA256,
     &webpki::RSA_PKCS1_2048_8192_SHA384,
     &webpki::RSA_PKCS1_2048_8192_SHA512,
@@ -99,7 +140,7 @@ pub fn verify_mra_cert(
     xt_signer_attn: &[u32],
     xt_signer: &[u8],
 ) -> Result<Vec<u8>, &'static str> {
-    debug!("verifyRA: start verifying RA cert");
+    print_utf8(b"verifyRA: start verifying RA cert");
     // Before we reach here, the runtime already verifed the extrinsic is properly signed by the extrinsic sender
     // Search for Public Key prime256v1 OID
     let prime256v1_oid = &[0x06, 0x08, 0x2A, 0x86, 0x48, 0xCE, 0x3D, 0x03, 0x01, 0x07];
@@ -125,7 +166,7 @@ pub fn verify_mra_cert(
     offset += 1;
     let pub_k = safe_indexing(cert_der, offset + 2, offset + len)?.to_vec(); // skip "00 04"
 
-    debug!("verifyRA public key: {:02x}", pub_k.iter().format(""));
+    print_utf8(b"verifyRA public key read");
 
     // Search for Netscape Comment OID
     let ns_cmt_oid = &[
@@ -140,7 +181,7 @@ pub fn verify_mra_cert(
     };
     offset += 12; // 11 + TAG (0x04)
 
-    debug!("netscape");
+    print_utf8(b"netscape");
     // Obtain Netscape Comment length
     let mut len = safe_indexing_one(cert_der, offset)? as usize;
 
@@ -173,7 +214,7 @@ pub fn verify_mra_cert(
         Err(_) => return Err("Bad DER"),
     };
 
-    debug!("intel cert");
+    print_utf8(b"intel cert");
     // Verify if the signing cert is issued by Intel CA
     let mut ias_ca_stripped = IAS_REPORT_CA.to_vec();
     ias_ca_stripped.retain(|&x| x != 0x0d && x != 0x0a);
@@ -186,8 +227,8 @@ pub fn verify_mra_cert(
         Err(_) => return Err("Decoding Error"),
     };
 
-    debug!("CA");
-    let mut ca_reader = BufReader::new(&IAS_REPORT_CA[..]);
+    print_utf8(b"CA");
+/*    let mut ca_reader = BufReader::new(&IAS_REPORT_CA[..]);
 
     let mut root_store = rustls::RootCertStore::empty();
     if root_store.add_pem_file(&mut ca_reader).is_err() {
@@ -203,7 +244,7 @@ pub fn verify_mra_cert(
     let mut chain: Vec<&[u8]> = Vec::new();
     chain.push(&ias_cert_dec);
 
-    let now_func = webpki::Time::try_from(SystemTime::now());
+    let now_func = webpki::Time::from_seconds_since_unix_epoch(1573419050);;
 
     match sig_cert.verify_is_valid_tls_server_cert(
         SUPPORTED_SIG_ALGS,
@@ -211,21 +252,21 @@ pub fn verify_mra_cert(
         &chain,
         now_func.unwrap(),
     ) {
-        Ok(_) => info!("Cert is good"),
-        Err(e) => error!("Cert verification error {:?}", e),
+        Ok(_) => print_utf8(b"Cert is good"),
+        Err(e) => print_utf8(b"Cert verification error"),
     }
-    debug!("verifyRA Intel Certificate is good");
+    print_utf8(b"verifyRA Intel Certificate is good");
 
     // Verify the signature against the signing cert
     match sig_cert.verify_signature(&webpki::RSA_PKCS1_2048_8192_SHA256, &attn_report_raw, &sig) {
-        Ok(_) => info!("Signature good"),
+        Ok(_) => print_utf8(b"Signature good"),
         Err(e) => {
-            error!("Signature verification error {:?}", e);
+            print_utf8(b"Signature verification error");
             return Err("Signature verification error");
         }
     }
-    debug!("verifyRA Intel signature is good");
-
+    print_utf8(b"verifyRA Intel signature is good");
+*/
     verify_attn_report(attn_report_raw, pub_k, xt_signer, xt_signer_attn)
 }
 
@@ -253,15 +294,11 @@ fn verify_attn_report(
         }
         _ => return Err("Failed to fetch timestamp from attestation report"),
     };
-    debug!(
-        "verifyRA attestation timestamp [unix epoch]: {}",
-        ra_timestamp
-    );
+    print_utf8(b"verifyRA attestation timestamp read");
 
     // get quote status (mandatory field)
     let ra_status = match &attn_report["isvEnclaveQuoteStatus"] {
         Value::String(quote_status) => {
-            info!("isvEnclaveQuoteStatus = {}", quote_status);
             match quote_status.as_ref() {
                 "OK" => SgxStatus::Ok,
                 "GROUP_OUT_OF_DATE" => SgxStatus::GroupOutOfDate,
@@ -272,7 +309,7 @@ fn verify_attn_report(
         }
         _ => return Err("Failed to fetch isvEnclaveQuoteStatus from attestation report"),
     };
-    debug!("verifyRA attestation status is: {:?}", ra_status);
+    print_utf8(b"verifyRA attestation status read");
 
     // parse quote body
     if let Value::String(quote_raw) = &attn_report["isvEnclaveQuoteBody"] {
@@ -280,36 +317,23 @@ fn verify_attn_report(
             Ok(q) => q,
             Err(_) => return Err("Quote Decoding Error"),
         };
-        debug!("Quote = {:?}", quote);
+        print_utf8(b"Quote read");
         // TODO: lack security check here
-        let sgx_quote: sgx_quote_t = unsafe { ptr::read(quote.as_ptr() as *const _) };
-
+        let sgx_quote: SgxQuote = match Decode::decode(&mut &quote[..]) {
+            Ok(q) => q,
+            Err(_) => return Err("could not decode quote")
+        };
         // Borrow of packed field is unsafe in future Rust releases
         // ATTENTION
         // DO SECURITY CHECK ON DEMAND
         // DO SECURITY CHECK ON DEMAND
         // DO SECURITY CHECK ON DEMAND
-        unsafe {
-            info!("sgx quote version = {}", sgx_quote.version);
-            info!("sgx quote signature type = {}", sgx_quote.sign_type);
-            info!(
-                "sgx quote report_data = {:02x}",
-                sgx_quote.report_body.report_data.d.iter().format("")
-            );
-            info!(
-                "sgx quote mr_enclave = {:02x}",
-                sgx_quote.report_body.mr_enclave.m.iter().format("")
-            );
-            info!(
-                "sgx quote mr_signer = {:02x}",
-                sgx_quote.report_body.mr_signer.m.iter().format("")
-            );
-        }
-        info!("Anticipated public key = {:02x}", pub_k.iter().format(""));
-        if sgx_quote.report_body.report_data.d.to_vec() == pub_k.to_vec() {
-            info!("Remote attestation of enclave successful!");
-        }
+        print_utf8(b"sgx quote parsed");
 
+        if sgx_quote.report_body.report_data.d.to_vec() == pub_k.to_vec() {
+            print_utf8(b"Remote attestation of enclave successful!");
+        }
+/*
         let ecc_handle = SgxEccHandle::new();
         let _result = ecc_handle.open();
 
@@ -336,14 +360,12 @@ fn verify_attn_report(
                 "wrong signature. Could not verify that the extrinsic signer is the enclave itself",
             );
         }
-        info!(
-            "extrinsic signer pubkey has been attested: {:02x}",
-            xt_signer.iter().format("")
-        );
+*/
+        print_utf8(b"extrinsic signer pubkey has been attested");
         let mut xt_signer_array = [0u8; 32];
         xt_signer_array.copy_from_slice(xt_signer);
         Ok(SgxReport {
-            mr_enclave: sgx_quote.report_body.mr_enclave.m,
+            mr_enclave: sgx_quote.report_body.mr_enclave,
             status: ra_status,
             pubkey: xt_signer_array,
             timestamp: ra_timestamp,
@@ -398,10 +420,6 @@ mod tests {
     const CERT_WRONG_SIG: &[u8] = b"0\x82\x0c\x8c0\x82\x0c2\xa0\x03\x02\x01\x02\x02\x01\x010\n\x06\x08*\x86H\xce=\x04\x03\x020\x121\x100\x0e\x06\x03U\x04\x03\x0c\x07MesaTEE0\x1e\x17\r190617124609Z\x17\r190915124609Z0\x121\x100\x0e\x06\x03U\x04\x03\x0c\x07MesaTEE0Y0\x13\x06\x07*\x86H\xce=\x02\x01\x06\x08*\x86H\xce=\x03\x01\x07\x03B\0\x04RT\x16\x16 \xef_\xd8\xe7\xc3\xb7\x03\x1d\xd6:\x1fF\xe3\xf2b!\xa9/\x8b\xd4\x82\x8f\xd1\xff[\x9c\x97\xbc\xf27\xb8,L\x8a\x01\xb0r;;\xa9\x83\xdc\x86\x9f\x1d%y\xf4;I\xe4Y\xc80'$K[\xd6\xa3\x82\x0bw0\x82\x0bs0\x82\x0bo\x06\t`\x86H\x01\x86\xf8B\x01\r\x04\x82\x0b`{\"id\":\"117077750682263877593646412006783680848\",\"timestamp\":\"2019-06-17T12:46:04.002066\",\"version\":3,\"isvEnclaveQuoteStatus\":\"GROUP_OUT_OF_DATE\",\"platformInfoBlob\":\"1602006504000900000909020401800000000000000000000008000009000000020000000000000B401A355B313FC939B4F48A54349C914A32A3AE2C4871BFABF22E960C55635869FC66293A3D9B2D58ED96CA620B65D669A444C80291314EF691E896F664317CF80C\",\"isvEnclaveQuoteBody\":\"AgAAAEALAAAIAAcAAAAAAOE6wgoHKsZsnVWSrsWX9kky0kWt9K4xcan0fQ996Ct+CAj//wGAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABwAAAAAAAAAHAAAAAAAAAFJJYIbPVot9NzRCjW2z9+k+9K8BsHQKzVMEHOR14hNbAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACD1xnnferKFHD2uvYqTXdDA8iZ22kCD5xw7h38CMfOngAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABSVBYWIO9f2OfDtwMd1jofRuPyYiGpL4vUgo/R/1ucl7zyN7gsTIoBsHI7O6mD3IafHSV59DtJ5FnIMCckS1vW\"}|EbPFH/ThUaS/dMZoDKC5EgmdUXUORFtQzF49Umi1P55oeESreJaUvmA0sg/ATSTn5t2e+e6ZoBQIUbLHjcWLMLzK4pJJUeHhok7EfVgoQ378i+eGR9v7ICNDGX7a1rroOe0s1OKxwo/0hid2KWvtAUBvf1BDkqlHy025IOiXWhXFLkb/qQwUZDWzrV4dooMfX5hfqJPi1q9s18SsdLPmhrGBheh9keazeCR9hiLhRO9TbnVgR9zJk43SPXW+pHkbNigW+2STpVAi5ugWaSwBOdK11ZjaEU1paVIpxQnlW1D6dj1Zc3LibMH+ly9ZGrbYtuJks4eRnjPhroPXxlJWpQ==|MIIEoTCCAwmgAwIBAgIJANEHdl0yo7CWMA0GCSqGSIb3DQEBCwUAMH4xCzAJBgNVBAYTAlVTMQswCQYDVQQIDAJDQTEUMBIGA1UEBwwLU2FudGEgQ2xhcmExGjAYBgNVBAoMEUludGVsIENvcnBvcmF0aW9uMTAwLgYDVQQDDCdJbnRlbCBTR1ggQXR0ZXN0YXRpb24gUmVwb3J0IFNpZ25pbmcgQ0EwHhcNMTYxMTIyMDkzNjU4WhcNMjYxMTIwMDkzNjU4WjB7MQswCQYDVQQGEwJVUzELMAkGA1UECAwCQ0ExFDASBgNVBAcMC1NhbnRhIENsYXJhMRowGAYDVQQKDBFJbnRlbCBDb3Jwb3JhdGlvbjEtMCsGA1UEAwwkSW50ZWwgU0dYIEF0dGVzdGF0aW9uIFJlcG9ydCBTaWduaW5nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqXot4OZuphR8nudFrAFiaGxxkgma/Es/BA+tbeCTUR106AL1ENcWA4FX3K+E9BBL0/7X5rj5nIgX/R/1ubhkKWw9gfqPG3KeAtIdcv/uTO1yXv50vqaPvE1CRChvzdS/ZEBqQ5oVvLTPZ3VEicQjlytKgN9cLnxbwtuvLUK7eyRPfJW/ksddOzP8VBBniolYnRCD2jrMRZ8nBM2ZWYwnXnwYeOAHV+W9tOhAImwRwKF/95yAsVwd21ryHMJBcGH70qLagZ7Ttyt++qO/6+KAXJuKwZqjRlEtSEz8gZQeFfVYgcwSfo96oSMAzVr7V0L6HSDLRnpb6xxmbPdqNol4tQIDAQABo4GkMIGhMB8GA1UdIwQYMBaAFHhDe3amfrzQr35CN+s1fDuHAVE8MA4GA1UdDwEB/wQEAwIGwDAMBgNVHRMBAf8EAjAAMGAGA1UdHwRZMFcwVaBToFGGT2h0dHA6Ly90cnVzdGVkc2VydmljZXMuaW50ZWwuY29tL2NvbnRlbnQvQ1JML1NHWC9BdHRlc3RhdGlvblJlcG9ydFNpZ25pbmdDQS5jcmwwDQYJKoZIhvcNAQELBQADggGBAGcIthtcK9IVRz4rRq+ZKE+7k50/OxUsmW8aavOzKb0iCx07YQ9rzi5nU73tME2yGRLzhSViFs/LpFa9lpQL6JL1aQwmDR74TxYGBAIi5f4I5TJoCCEaRHz91kpG6Uvyn2tLmnIdJbPE4vYvWLrtXXfFBSSPD4Afn7+3/XUggAlc7oCTizOfbbtOFlYA4g5KcYgS1J2ZAeMQqbUdZseZCcaZZZn65tdqee8UXZlDvx0+NdO0LR+5pFy+juM0wWbu59MvzcmTXbjsi7HY6zd53Yq5K244fwFHRQ8eOB0IWB+4PfM7FeAApZvlfqlKOlLcZL2uyVmzRkyR5yW72uo9mehX44CiPJ2fse9Y6eQtcfEhMPkmHXI01sN+KwPbpA39+xOsStjhP9N1Y1a2tQAVo+yVgLgV2Hws73Fc0o3wC78qPEA+v2aRs/Be3ZFDgDyghc/1fgU+7C+P6kbqd4poyb6IW8KCJbxfMJvkordNOgOUUxndPHEi/tb/U7uLjLOgPA==0\n\x06\x08*\x86H\xce=\x04\x03\x02\x03H\00E\x02!\0\xae6\x06\t@Sy\x8f\x8ec\x9d\xdci^Ex*\x92}\xdcG\x15A\x97\xd7\xd7\xd1\xccx\xe0\x1e\x08\x02 \x15Q\xa0BT\xde'~\xec\xbd\x027\xd3\xd8\x83\xf7\xe6Z\xc5H\xb4D\xf7\xe2\r\xa7\xe4^f\x10\x85p";
     const CERT_TOO_SHORT1: &[u8] = b"0\x82\x0c\x8c0\x82\x0c2\xa0\x03\x02\x01\x02\x02\x01\x010\n\x06\x08*\x86H\xce=\x04\x03\x020\x121\x100\x0e\x06\x03U\x04\x03\x0c\x07MesaTEE0\x1e\x17\r190617124609Z\x17\r190915124609Z0\x121\x100\x0e\x06\x03U\x04\x03\x0c\x07MesaTEE0Y0\x13\x06\x07*\x86H\xce=\x02\x01\x06\x08*\x86H\xce=\x03\x01\x07\x03B\0\x04RT\x16\x16 \xef_\xd8\xe7\xc3\xb7\x03\x1d\xd6:\x1fF\xe3\xf2b!\xa9/\x8b\xd4\x82\x8f\xd1\xff[\x9c\x97\xbc\xf27\xb8,L\x8a\x01\xb0r;;\xa9\x83\xdc\x86\x9f\x1d%y\xf4;I\xe4Y\xc80'$K[\xd6\xa3\x82\x0bw0\x82\x0bs0\x82\x0bo\x06\t`\x86H\x01\x86\xf8B\x01\r\x04\x82\x0b`{\"id\":\"117077750682263877593646412006783680848\",\"timestamp\":\"2019-06-17T12:46:04.002066\",\"version\":3,\"isvEnclaveQuoteStatus\":\"GROUP_OUT_OF_DATE\",\"platformInfoBlob\":\"1602006504000900000909020401800000000000000000000008000009000000020000000000000B401A355B313FC939B4F48A54349C91\x03\x02\x03H\00E\x02!\0\xae6\x06\t@Sy\x8f\x8ec\x9d\xdci^Ex*\x92}\xdcG\x15A\x97\xd7\xd7\xd1\xccx\xe0\x1e\x08\x02 \x15Q\xa0BT\xde'~\xec\xbd\x027\xd3\xd8\x83\xf7\xe6Z\xc5H\xb4D\xf7\xe2\r\xa7\xe4^f\x10\x85p";
     const CERT_TOO_SHORT2: &[u8] = b"0\x82\x0c\x8c0";
-    // this is a hack to allow logging during test runs
-    fn init() {
-        let _ = env_logger::builder().is_test(true).try_init();
-    }
 
     #[test]
     fn verify_mra_cert_should_work() {
@@ -457,7 +475,6 @@ mod tests {
 
     #[test]
     fn verify_short_cert_is_err() {
-        init();
         let signer_attn: [u32; 16] = Decode::decode(&mut TEST1_SIGNER_ATTN).unwrap();
         assert!(verify_mra_cert(CERT_TOO_SHORT1, &signer_attn, TEST1_SIGNER_PUB).is_err());
         assert!(verify_mra_cert(CERT_TOO_SHORT2, &signer_attn, TEST1_SIGNER_PUB).is_err());
