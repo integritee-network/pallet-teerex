47b24fe0c4bd254206e956690e317f9154564d03
Christian Langenbacher <christian.langenbacher@scs.ch>
2020-05-28 13:14:39 +0200

    bump version: 0.6.8-sub2.0.0-alpha.7

--- src/lib.rs
+++ src/lib.rs
@@ -193,7 +193,7 @@ decl_module! {
                 T::Currency::transfer(&bonding_account, &public_account, amount, ExistenceRequirement::AllowDeath)?;
                 <ConfirmedCalls>::insert(call_hash, 0);
                 Self::deposit_event(RawEvent::UnshieldedFunds(public_account));
-            } {
+            } else {
                 native::info!("Second confirmation for call: {:?}", call_hash);
             }
 
