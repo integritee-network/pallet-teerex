0748e71d06cf9d6c8573e77b6659eb7c44596716
brenzi <brenzi@users.noreply.github.com>
2021-03-09 15:32:16 +0100

    upgraded to 743accbe and refactored rest mock to use construct_runtime (#19)

--- src/mock.rs
+++ src/mock.rs
@@ -1,69 +1,72 @@
 // Creating mock runtime here
-
-use crate::{Module, Trait};
-use frame_support::{impl_outer_event, impl_outer_origin, parameter_types, weights::Weight};
+use crate as substratee_registry;
+use substratee_registry::Config;
+use frame_support::parameter_types;
 use frame_system as system;
 use sp_core::{sr25519, H256};
 use sp_keyring::AccountKeyring;
 use sp_runtime::{
     testing::Header,
-    traits::{BlakeTwo256, IdentityLookup, Verify},
-    Perbill,
+    traits::{BlakeTwo256, IdentityLookup, Verify}
 };
 
-impl_outer_origin! {
-    pub enum Origin for TestRuntime {}
-}
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
+type Block = frame_system::mocking::MockBlock<Test>;
 
-#[derive(Clone, Eq, PartialEq)]
-pub struct TestRuntime;
+frame_support::construct_runtime!(
+    pub enum Test where
+        Block = Block,
+        NodeBlock = Block,
+        UncheckedExtrinsic = UncheckedExtrinsic,
+    {
+        System: frame_system::{Module, Call, Config, Storage, Event<T>},
+        Balances: pallet_balances::{Module, Call, Storage, Config<T>, Event<T>},
+        Timestamp: timestamp::{Module, Call, Storage, Inherent},
+        SubstrateeRegistry: substratee_registry::{Module, Call, Storage, Event<T>},
+    }
+);
+
 parameter_types! {
     pub const BlockHashCount: u64 = 250;
-    pub const MaximumBlockWeight: Weight = 1024;
-    pub const MaximumBlockLength: u32 = 2 * 1024;
-    pub const AvailableBlockRatio: Perbill = Perbill::from_percent(75);
 }
-impl system::Trait for TestRuntime {
+impl frame_system::Config for Test {
     type BaseCallFilter = ();
+    type BlockWeights = ();
+    type BlockLength = ();
+    type DbWeight = ();
     type Origin = Origin;
-    type Call = ();
     type Index = u64;
+    type Call = Call;
     type BlockNumber = u64;
     type Hash = H256;
     type Hashing = BlakeTwo256;
     type AccountId = AccountId;
     type Lookup = IdentityLookup<Self::AccountId>;
     type Header = Header;
-    type Event = TestEvent;
+    type Event = Event;
     type BlockHashCount = BlockHashCount;
-    type MaximumBlockWeight = MaximumBlockWeight;
-    type DbWeight = ();
-    type BlockExecutionWeight = ();
-    type ExtrinsicBaseWeight = ();
-    type MaximumBlockLength = MaximumBlockLength;
-    type MaximumExtrinsicWeight = MaximumBlockWeight;
-    type AvailableBlockRatio = AvailableBlockRatio;
     type Version = ();
-    type AccountData = balances::AccountData<Balance>;
+    type PalletInfo = PalletInfo;
+    type AccountData = pallet_balances::AccountData<Balance>;
     type OnNewAccount = ();
     type OnKilledAccount = ();
     type SystemWeightInfo = ();
-    type PalletInfo = ();
+    type SS58Prefix = ();
 }
 
 pub type Balance = u64;
 
 parameter_types! {
-    pub const ExistentialDeposit: u64 = 500;
+    pub const ExistentialDeposit: u64 = 1;
 }
 
-impl balances::Trait for TestRuntime {
-    type Balance = Balance;
-    type Event = TestEvent;
+impl pallet_balances::Config for Test {
+    type MaxLocks = ();
+    type Balance = u64;
     type DustRemoval = ();
+    type Event = Event;
     type ExistentialDeposit = ExistentialDeposit;
     type AccountStore = System;
-    type MaxLocks = ();
     type WeightInfo = ();
 }
 
@@ -73,7 +76,7 @@ parameter_types! {
 
 pub type Moment = u64;
 
-impl timestamp::Trait for TestRuntime {
+impl timestamp::Config for Test {
     type Moment = Moment;
     type OnTimestampSet = ();
     type MinimumPeriod = MinimumPeriod;
@@ -84,25 +87,25 @@ parameter_types! {
     pub const MomentsPerDay: u64 = 86_400_000; // [ms/d]
 }
 
-impl Trait for TestRuntime {
-    type Event = TestEvent;
+impl Config for Test {
+    type Event = Event;
     type Currency = Balances;
     type MomentsPerDay = MomentsPerDay;
 }
 
 // Easy access alias
-pub type Registry = Module<TestRuntime>;
-pub type System = system::Module<TestRuntime>;
-pub type Balances = balances::Module<TestRuntime>;
-pub type Timestamp = timestamp::Module<TestRuntime>;
+//pub type Registry = Module<Test>;
+//pub type System = system::Module<TestRuntime>;
+//pub type Balances = balances::Module<TestRuntime>;
+//pub type Timestamp = timestamp::Module<TestRuntime>;
 
 // This function basically just builds a genesis storage key/value store according to
 // our desired mockup.
 pub fn new_test_ext() -> sp_io::TestExternalities {
     let mut t = system::GenesisConfig::default()
-        .build_storage::<TestRuntime>()
+        .build_storage::<Test>()
         .unwrap();
-    balances::GenesisConfig::<TestRuntime> {
+    pallet_balances::GenesisConfig::<Test> {
         balances: vec![(AccountKeyring::Alice.public(), 1 << 60)],
     }
     .assimilate_storage(&mut t)
@@ -116,15 +119,3 @@ pub fn new_test_ext() -> sp_io::TestExternalities {
 pub type Signature = sr25519::Signature;
 /// An identifier for an account on this system.
 pub type AccountId = <Signature as Verify>::Signer;
-
-mod registry {
-    pub use crate::Event;
-}
-
-impl_outer_event! {
-    pub enum TestEvent for TestRuntime {
-        registry<T>,
-        system<T>,
-        balances<T>,
-    }
-}
