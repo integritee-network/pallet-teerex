2ab5b4da3d28ef37b57ad58dbbd1a23370042f95
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-07-07 16:48:32 +0200

    [test-utils] fix no std

--- src/test_utils.rs
+++ src/test_utils.rs
@@ -1,13 +1,12 @@
 #![cfg(any(test, feature = "runtime-benchmarks"))]
 
 use frame_benchmarking::sp_std::convert::TryFrom;
+use sp_std::fmt::Debug;
 
 pub fn get_signer<'a, AccountId>(pubkey: &'a [u8]) -> AccountId
 where
-
-	AccountId: TryFrom<&'a [u8]>,
-	// Todo: Check what's up with this weird trait bounds
-	<AccountId as TryFrom<&'a [u8]>>::Error: std::fmt::Debug
+    AccountId: TryFrom<&'a [u8]>,
+    <AccountId as TryFrom<&'a [u8]>>::Error: Debug,
 {
-	AccountId::try_from(pubkey).unwrap()
+    AccountId::try_from(pubkey).unwrap()
 }
