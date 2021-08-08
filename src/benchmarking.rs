354f3642d8e765c1549d61adf96a3dfb03362384
clangenb <37865735+clangenb@users.noreply.github.com>
2021-08-08 19:13:38 +0200

    assume ra_report is plain mrenclave when compiled with `skip-ias-check` flag (#33)
    This makes the M8-/M6-integration test pass with `skip-ra` pass.

--- src/benchmarking.rs
+++ src/benchmarking.rs
@@ -30,6 +30,7 @@ use crate::test_utils::{consts::URL, get_signer, ias::TEST4_SETUP};
 use crate::Pallet as Teerex;
 
 fn ensure_not_skipping_ra_check() {
+    #[cfg(not(test))]
     if cfg!(feature = "skip-ias-check") {
         panic!("Benchmark does not allow the `skip-ias-check` flag.");
     };
@@ -39,7 +40,7 @@ fn generate_accounts<T: Config>(amount: u32) -> Vec<T::AccountId> {
     (0..amount).map(|n| account("dummy name", n, n)).collect()
 }
 
-fn add_enclaves_to_registry<T: Config>(accounts: &Vec<T::AccountId>) {
+fn add_enclaves_to_registry<T: Config>(accounts: &[T::AccountId]) {
     for a in accounts.iter() {
         Teerex::<T>::add_enclave(a, &Enclave::default().with_pubkey(a.clone())).unwrap()
     }
