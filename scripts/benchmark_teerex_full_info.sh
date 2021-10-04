57fe161377cc0e5f814716630aeaf0f56ef93f23
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-10-04 06:47:53 +0200

    absolute paths work for configs and output

--- scripts/benchmark_teerex_full_info.sh
+++ scripts/benchmark_teerex_full_info.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-#set -eo pipefail
-
-#usage() {
-#    echo Usage:
-#    echo "$0 <node-binary>"
-#    echo "$1 <chain-id>"
-#    exit 1
-#}
+set -eo pipefail
 
 # This creates and extended weight file that contains:
 # * `WeightInfo` trait declaration
@@ -16,33 +9,27 @@
 # The output file is intended to be used in the `pallet_teerex` internally for development only. It contains more
 # information than needed for the actual node.
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-PROJ_ROOT="$(dirname "$SCRIPT_DIR")"
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PROJ_ROOT="$(dirname "$SCRIPTS_DIR")"
+TEEREX_SRC="$PROJ_ROOT/src"
 
-# use absolute path for the output file
-TEEREX_SRC_DIR="$PROJ_ROOT/src"
+echo "PROJ_ROOT:    $SCRIPTS_DIR"
+echo "SCRIPTS_DIR:  $PROJ_ROOT"
+echo "TEEREX_SRC:   $TEEREX_SRC"
 
-echo "SCRIPT_DIR:     ${SCRIPT_DIR}"
-echo "PROJ_ROOT:      ${PROJ_ROOT}"
-echo "TEEREX_SRC_DIR: ${TEEREX_SRC_DIR}"
+NODE_BINARY="../integritee-node/target/release/integritee-node"
+#CHAIN_SPEC=$
 
-NODE_BINARY=${0}
-CHAIN_SPEC=${1}
+$NODE_BINARY \
+  benchmark \
+  --chain=integritee-mainnet \
+  --steps=50 \
+  --repeat=20 \
+  --pallet=pallet_teerex \
+  --extrinsic="*" \
+  --execution=wasm \
+  --wasm-execution=compiled \
+  --heap-pages=4096 \
+  --output="$TEEREX_SRC"/weights.rs \
+  --template="$SCRIPTS_DIR"/frame-weight-template-full-info.hbs
 
-echo "Creating weight definitinos for pallet_teerex"
-echo "node:   ${NODE_BINARY}"
-echo "chain:  ${CHAIN_SPEC}"
-
-#$NODE_BINARY \
-#  benchmark \
-#  --chain="$CHAIN_SPEC" \
-#  --steps=50 \
-#  --repeat=20 \
-#  --pallet=pallet_teerex \
-#  --extrinsic="*" \
-#  --execution=wasm \
-#  --wasm-execution=compiled \
-#  --heap-pages=4096 \
-#  --output="$TEEREX_SRC_DIR/weights.rs" \
-#  --template="$SCRIPT_DIR"/frame-weight-template-full-info.hbs
-#
